053a7f83e2dd77d8d6ad350e3628ca5246a16a23
Jon-Luke West <1074249+jldubz@users.noreply.github.com>
2019-02-07 21:40:55 -0500

    Document LoadMoreViewHolder class

--- app/src/main/java/com/jldubz/gistaviewer/ui/gists/LoadMoreViewHolder.java
+++ app/src/main/java/com/jldubz/gistaviewer/ui/gists/LoadMoreViewHolder.java
@@ -5,6 +5,9 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+/***
+ * Displays a circular, indeterminate progress bar in a RecyclerView to indicate that more information can be loaded
+ */
 public class LoadMoreViewHolder extends RecyclerView.ViewHolder {
 
     public LoadMoreViewHolder(@NonNull View itemView) {
