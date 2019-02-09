54c64ff4fc950fb12ec91a64af847a7396b89ea5
Jon-Luke West <1074249+jldubz@users.noreply.github.com>
2019-02-09 16:30:06 -0500

    Document and add proguard @Keep tag

--- app/src/main/java/com/jldubz/gistaviewer/ui/MainActivity.java
+++ app/src/main/java/com/jldubz/gistaviewer/ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.jldubz.gistaviewer.ui;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.jldubz.gistaviewer.R;
@@ -11,12 +10,20 @@ import com.jldubz.gistaviewer.ui.gists.StarGistsFragment;
 import com.jldubz.gistaviewer.ui.gists.YourGistsFragment;
 import com.jldubz.gistaviewer.viewmodel.MainViewModel;
 
+import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+/**
+ * Used to host a bottom navigation bar for a DiscoverGistsFragment, StarGistsFragment, YourGistsFragment,
+ *  and ProfileFragment
+ *
+ * @author Jon-Luke West
+ */
+@Keep
 public class MainActivity extends AppCompatActivity {
 
     private static final String KEY_SELECTED_TAB_ID = "com.jldubz.gistaviewer.MainActivity.tab";
@@ -34,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get the shared view model
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        //Get any saved credentials
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.key_pref_file), MODE_PRIVATE);
         String username = sharedPreferences.getString(getString(R.string.key_pref_username), "");
         String token = sharedPreferences.getString(getString(R.string.key_pref_token), "");
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             viewModel.logIn(username, token);
         }
 
+        //Resume from the last tab that was open
         if (savedInstanceState != null) {
             mSelectedTabId = savedInstanceState.getInt(KEY_SELECTED_TAB_ID);
             if (mSelectedTabId <= 0) {
@@ -50,10 +60,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //Config the bottom nav view
         BottomNavigationView bottomNavigation = findViewById(R.id.navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
-            Log.d("Gist", "tab selected " + item.getTitle());
+            //Log.d("Gist", "tab selected " + item.getTitle());
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction;
             switch (itemId) {
@@ -81,16 +92,19 @@ public class MainActivity extends AppCompatActivity {
             return false;
         });
 
+        //Make new instances of the fragments
         mDiscoverGistsFragment = DiscoverGistsFragment.newInstance();
         mStarGistsFragment = StarGistsFragment.newInstance();
         mYourGistsFragment = YourGistsFragment.newInstance();
         mProfileFragment = ProfileFragment.newInstance();
 
+        //Set the currently selected tab
         bottomNavigation.setSelectedItemId(mSelectedTabId);
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        //Save the currently selected tab for resuming later
         outState.putInt(KEY_SELECTED_TAB_ID, mSelectedTabId);
         super.onSaveInstanceState(outState);
     }
